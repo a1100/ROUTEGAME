cf9e110894b008ee109d254bbea31888fb1f3d24
a1100 <ag49715@students.westport.k12.ct.us>
2015-01-29 12:11:02 -0500

    Initial Commit

---
+++ routegame.rb
@@ -0,0 +1,48 @@
+require 'bundler'
+Bundler.require
+
+# number guessing game
+playerpts, computerpts = 0, 0
+invalid_enters = 0
+get '/guess/:number' do
+  player = params[:number].to_i
+  numbers = (1..20).to_a
+  computer = numbers.sample.to_i
+  random = numbers.sample.to_i
+  playerdiff = (player - random).abs
+  compdiff = (computer - random).abs
+  winner = nil
+  if numbers.include?(player) == false
+    invalid_enters += 1
+    winner = "computer because player did not choose a number from 1-20"
+    if invalid_enters > 1
+      playerpts -= 1
+    end
+  else
+    if playerdiff == compdiff
+      winner = "it's a tie!"
+    elsif playerdiff < compdiff
+      winner = "player"
+      if player == random
+        playerpts += 2
+      else
+        playerpts += 1
+      end
+    elsif compdiff < playerdiff
+      winner = "computer"
+        if computer == random
+          computerpts += 2
+        else
+          computerpts += 1
+        end
+    end
+  end
+  "Guesses: <br> Player: #{player} | Computer: #{computer}  <br> <br> The winner is...#{winner}!!! <br> The random number was #{random}. <br> <br> Points: <br> Player: #{playerpts} | Computer: #{computerpts} <br> <br> Note: you receive one point for winning a round, and two for exactly guessing the number. <br> Also if you invalidly guess a number more than once, you will lose a point."
+end
+
+get '/play/:game' do
+  "You can play games from this page. Enter the name of the game after the /play in the url. <br> <br> Options: <br> guess/(number)"
+end
+
+
+
